c842f2fb96b10685df4d54e86eccd47838ac3697
wayqui <joselobustamante@gmail.com>
2020-07-17 02:11:52 +0200

    Update ArrayManipulationTest.java
    Adding tests regarding array and map functions and algorithms

--- src/test/java/com/wayqui/hackerrank/arrays/array_manipulation/ArrayManipulationTest.java
+++ src/test/java/com/wayqui/hackerrank/arrays/array_manipulation/ArrayManipulationTest.java
@@ -9,6 +9,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.OptionalLong;
+import com.wayqui.demo.dto.PersonDto;
+import com.wayqui.demo.entity.Person;
+import com.wayqui.demo.mapper.PersonMapper;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -112,5 +124,132 @@ public class ArrayManipulationTest {
             System.out.print(array[i]+" ");
         }
         System.out.println();
+    }
+    
+    @Test
+    public void repeated_string() {
+        String s = "aedbdaeaddadddcedcbbabdccbecaecaccdbebeeadadcaabbaabbaeeeecaddbcdecbbdccdebaaebecdaaabbcdeccbabaabce";
+        long n = 731869010806L;
+
+        
+        long repetitions = n / s.length();
+        long remaining = n % s.length();
+
+        long repeatedAes = s.chars().filter(ch -> ch == 'a').count() * repetitions;
+        repeatedAes = repeatedAes + s.substring(0, (int) remaining)
+                .chars().filter(ch -> ch == 'a').count();
+
+        System.out.println("s: "+s);
+        System.out.println("s (size): "+s.length());
+        System.out.println("n: "+n);
+        System.out.println("#aes in s: "+s.chars().filter(ch -> ch == 'a').count());
+        System.out.println("repetitions: "+repetitions);
+        System.out.println("remaining: "+remaining);
+        System.out.println("repeatedAes: "+repeatedAes);
+
+        assertThat(repeatedAes).isEqualTo(168329872486L);
+    }
+
+    @Test
+    public void makeAnagrams() {
+        assertThat(MockData.makeAnagram("cde", "dcff")).isEqualTo(3);
+    }
+
+    // Complete the makeAnagram function below.
+    static int makeAnagram(String a, String b) {
+        a = a.trim();
+        b = b.trim();
+        if (a.equals(b)) {
+            //System.out.println("The strings are equal");
+            return 0;
+        }
+        String sortedA = a.chars().sorted().toString();
+        String sortedB = b.chars().sorted().toString();
+        if (sortedA.equals(sortedB)) {
+            //System.out.println("The sorted strings are equal");
+            return 0;
+        }
+
+        Map<Character, Integer> aStatistics = countCharsRepetitions(a);
+        Map<Character, Integer> bStatistics = countCharsRepetitions(b);
+
+        System.out.println("Frequencies a:\n" + aStatistics);
+        System.out.println("Frequencies b:\n" + bStatistics);
+
+        if (aStatistics == bStatistics) {
+            //System.out.println("The statistics for both strings are equal");
+            return 0;
+        }
+
+        Integer minRemovals = findDifferences(aStatistics, bStatistics);
+        minRemovals += findDifferences(bStatistics, aStatistics);
+
+        return minRemovals;
+    }
+
+    private static Integer findDifferences(Map<Character, Integer> aStatistics, Map<Character, Integer> bStatistics) {
+        AtomicReference<Integer> counting = new AtomicReference<>(0);
+
+        aStatistics.forEach((k , v) -> {
+            if (bStatistics.get(k) == null) {
+                counting.getAndSet(counting.get() + v);
+            } else if (bStatistics.get(k) > v) {
+                counting.getAndSet(counting.get() + (bStatistics.get(k) - v));
+            }
+        });
+        return counting.get();
+    }
+
+    private static Map<Character, Integer> countCharsRepetitions(String a) {
+        return a.chars().boxed()
+                .collect(toMap(
+                        k -> (char) k.intValue(),
+                        v -> 1,
+                        Integer::sum));
+    }
+
+    @Test
+    public void alternatingCharacters() {
+        assertThat(MockData.alternatingCharacters("AAAA")).isEqualTo(3);
+        assertThat(MockData.alternatingCharacters("BBBBB")).isEqualTo(4);
+        assertThat(MockData.alternatingCharacters("ABABABAB")).isEqualTo(0);
+        assertThat(MockData.alternatingCharacters("BABABA")).isEqualTo(0);
+        assertThat(MockData.alternatingCharacters("AAABBB")).isEqualTo(4);
+    }
+
+    static int alternatingCharacters(String s) {
+        char[] charArray = s.toCharArray();
+        int erased = 0;
+        for (int i = 0; i < charArray.length-1; i++) {
+            if (charArray[i] == charArray[i+1]) erased++;
+        }
+        return erased;
+    }
+
+    @Test
+    public void isValid() {
+        assertThat(MockData.isValid("abbccc")).isEqualTo("YES");
+        //assertThat(MockData.isValid("abcc")).isEqualTo("YES");
+        //assertThat(MockData.isValid("abccc")).isEqualTo("NO");
+        //assertThat(MockData.isValid("abcdefghhgfedecba")).isEqualTo("YES");
+        //assertThat(MockData.isValid("abcdefghhgfedecbaf")).isEqualTo("NO");
+    }
+
+    static String isValid(String s) {
+        Map<Character, Integer> frequencies = countCharsRepetitions(s);
+
+        List<Integer> sortedFreq = frequencies.values().stream().sorted().collect(Collectors.toList());
+
+
+
+
+
+        List<Integer> result = frequencies.values().stream().distinct().collect(Collectors.toList());
+
+        long distinctChars = s.trim().toLowerCase().chars().boxed().distinct().count();
+        if (distinctChars == 0) return "YES";
+        long extraCharacters = s.length() % distinctChars;
+        if (extraCharacters > 1) return "NO";
+        return "YES";
     }
 }
